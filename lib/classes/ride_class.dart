fa30a1fa964c9a92db400141acf10393d102bff2
theshortsmonkey <j.horsley72@gmail.com>
2024-07-10 07:54:49 +0100

    updated ride class to return all details from database

--- lib/classes/ride_class.dart
+++ lib/classes/ride_class.dart
@@ -3,11 +3,19 @@ enum SeatsLabel {
   two('2', 2),
   three('3', 3),
   four('4', 4),
-  five('5', 5);
+  five('5', 5),
+  six('6', 6),
+  seven('7', 7),
+  eight('8', 8);
 
-  const SeatsLabel(this.label, this.seats);
   final String label;
   final int seats;
+  
+  const SeatsLabel(this.label, this.seats);
+
+  factory SeatsLabel.fromInt(int seats) {
+    return values.firstWhere((e) => e.seats == seats);
+  }
 }
 
 enum RegionsLabel {
@@ -15,11 +23,17 @@ enum RegionsLabel {
   two('North East', 'North East'),
   three('Yorkshire', 'Yorkshire'),
   four('Midlands', 'Midlands'),
-  five('South', 'South');
+  five('South', 'South'),
+  six('Scotland', 'Scotland');
 
-  const RegionsLabel(this.label, this.region);
   final String label;
   final String region;
+
+  const RegionsLabel(this.label, this.region);
+
+  factory RegionsLabel.fromString(String region) {
+    return values.firstWhere((e) => e.region == region);
+  }
 }
 
 class Ride {
@@ -38,7 +52,7 @@ class Ride {
   final int price;
   final dynamic map;
   final DateTime? setDateTime; 
-  final String? getDateTime;
+  final String getDateTime;
   final dynamic chats;
   final int? driverRating;
 
@@ -59,7 +73,7 @@ class Ride {
     this.price = 0,
     this.map,
     this.setDateTime,
-    this.getDateTime,
+    this.getDateTime = '2024-01-01T00:00:01.000Z',
     this.chats = const [],
   });
 
@@ -81,11 +95,15 @@ class Ride {
       };
   
   factory Ride.fromJson(Map<String, dynamic> json) {
+    RegionsLabel getToRegion = RegionsLabel.fromString(json['to_region'] as String);
+    RegionsLabel getFromRegion = RegionsLabel.fromString(json['from_region'] as String);
     return Ride( 
       id: json['id'] as String,
       driverUsername: json['driver_username'] as String,
       to: json['to'] as String,
+      toRegion: getToRegion,
       from: json['from'] as String,
+      fromRegion: getFromRegion,
       riderUsernames: json['rider_usernames'] as List<dynamic>,
       jumpInRequests: json['jumpin_requests'] as List<dynamic>,
       carbonEmissions: json['carbon_emissions'] as int,
@@ -94,6 +112,8 @@ class Ride {
       driverRating: json['driver_rating'] as int,
       getDateTime: json['date_and_time'] as String,
       getAvailableSeats: json['available_seats'] as int,
+      map: json['map'] as Map<String, dynamic>,
+      chats: json['chats'] as List<dynamic>
     );
   }
   static List<Ride> fromJsonList(List<Map<String, dynamic>> jsonList) { 
