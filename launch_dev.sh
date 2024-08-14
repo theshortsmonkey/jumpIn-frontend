4a692c5d04625239e26417d67de81926fba34fbe
theshortsmonkey <j.horsley72@gmail.com>
2024-08-14 13:23:01 +0100

    updated api paths, removed unnessecary packages, updated launch scripts

---
+++ launch_dev.sh
@@ -0,0 +1,5 @@
+
+#'localhost:1337'
+#'jumpin-backend.onrender.com'
+
+flutter run --dart-define=API_URL='jumpin-backend.onrender.com' -d chrome --web-port=43089
--- lib/utils/api_users.dart
+++ lib/utils/api_users.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:fe/utils/api_paths.dart';
 import 'package:fe/utils/process_api_response.dart';
-import "package:http/http.dart" as http;
-import 'package:enhanced_http/enhanced_http.dart';
+import 'package:flutter/material.dart';
+import "package:http/http.dart";
 import 'package:fe/auth_provider.dart';
 import "package:fe/classes/user_class.dart";
 
@@ -19,40 +19,40 @@ Future<ActiveSession> postLogin(String username, String password) async {
   final bodyJson = jsonEncode({'password': password});
   Uri url = Uri.parse('$baseUrl/users/$username/login');
   try {
-    final response = await clientWithCredentials.post(url, body: bodyJson,);
-    final result = ActiveSession.fromJson(jsonDecode(processResponse(response)) as Map<String, dynamic>);
+    final response = await clientWithCredentials.post(
+      url,
+      body: bodyJson,
+    );
+    final result = ActiveSession.fromJson(
+        jsonDecode(processResponse(response)) as Map<String, dynamic>);
     return result;
   } on ClientException {
     throw Exception('server unavailable');
   } catch (e) {
     throw Exception(e.toString());
-  } 
+  }
 }
 
 Future<void> postLogout(String username) async {
   Uri url = Uri.parse('$baseUrl/users/$username/logout');
-  final response = await http.post(url);
+  final response = await clientWithCredentials.post(url);
   processResponse(response);
 }
 
 Future<List<User>> fetchUsers() async {
-    Uri url = Uri.parse('$baseUrl/users');
-    final response = await clientWithCredentials.get(url);
-    List<User> users = jsonDecode(processResponse(response)).map<User>((item) {
-      return User.fromJson(item as Map<String, dynamic>);
-    }).toList();
-    return users;
+  Uri url = Uri.parse('$baseUrl/users');
+  final response = await clientWithCredentials.get(url);
+  List<User> users = jsonDecode(processResponse(response)).map<User>((item) {
+    return User.fromJson(item as Map<String, dynamic>);
+  }).toList();
+  return users;
 }
 
 Future<User> fetchUserByUsername(username) async {
-  try {
     Uri url = Uri.parse('$baseUrl/users/$username');
     final response = await clientWithCredentials.get(url);
     var user = User.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
     return user;
-  } catch (e) {
-    throw Exception('No users found');
-  }
 }
 
 Future<User> postUser(user) async {
@@ -78,29 +78,24 @@ Future<User> patchUser(user) async {
 
 Future fetchCarDetails(carReg) async {
   try {
-    Uri url = Uri.parse(
-        'https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles');
-    final response = await http.post(
+    Uri url = Uri.parse(licenseURL);
+    final response = await clientWithCredentials.post(
       url,
-      headers: {
-        'x-api-key': '1gZwZ4vfFN1TbScqIP7FG4ccTa8SkB95aJN9wHBs',
-        "accept": '*/*',
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD"
-      },
+      headers: licenseHeaders,
       body: jsonEncode({'registrationNumber': carReg}),
     );
     final result = json.decode(processResponse(response));
     return result;
   } catch (e) {
+    debugPrint(e.toString());
     throw Exception("Error fetching car details: $e");
   }
 }
 
 Future<void> uploadUserProfilePic(String username, String filePath) async {
   Uri url = Uri.parse('$baseUrl/users/$username/image');
-  final response =
-      await clientWithCredentials.post(url, body: jsonEncode({'filePath': filePath}));
+  final response = await clientWithCredentials.post(url,
+      body: jsonEncode({'filePath': filePath}));
   return processResponse(response);
 }
 
